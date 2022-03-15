111b173c95e230c2222e04c6b3b891cc3131de91
Ilana Shapiro <ilanashapiro@Ilanas-MacBook-Pro.local>
2022-03-14 22:38:31 -0700

    CHORDS WORK

--- app/MusicXMLgen.hs
+++ app/MusicXMLgen.hs
@@ -124,11 +124,13 @@ generateRestsFromDivisions restDurationValPairs = return $
   restDurationValPairs
 
 -- all these notes will be tied BOTH WAYS
-generateTiedNotesFromDivisions :: [CodeLine] -> [(MusAST.Duration, Int)] -> IO [CodeLine]
-generateTiedNotesFromDivisions pitchCode noteDurationValPairs = return $ 
+generateTiedNotesFromDivisions :: [[CodeLine]] -> [(MusAST.Duration, Int)] -> IO [CodeLine]
+generateTiedNotesFromDivisions pitchesCode noteDurationValPairs = return $ 
     Prelude.concatMap (\(noteDuration, noteVal) ->
       let noteTypeCode = durationToNoteTypeCode noteDuration
-      in ["\t\t\t<note>"]
+      in Prelude.concatMap 
+          (\pitchCode -> 
+            ["\t\t\t<note>"]
               ++ pitchCode ++
               ["\t\t\t\t<duration>" ++ show noteVal ++ "</duration>", -- the duration of this note is all the time that's left in the measure, since the note doesn't fit in the measure
               "\t\t\t\t<tie type=\"start\"/>",
@@ -139,8 +141,8 @@ generateTiedNotesFromDivisions pitchCode noteDurationValPairs = return $
                 "\t\t\t\t\t<tied type=\"start\"/>",
                 "\t\t\t\t\t<tied type=\"stop\"/>",
               "\t\t\t\t</notations>",
-            "\t\t\t</note>"])
-    noteDurationValPairs
+            "\t\t\t</note>"]) pitchesCode)
+          noteDurationValPairs
 
 -----------------------------------------------------------------------------------------
 -- Code Generation for Musical Expressions
@@ -191,37 +193,48 @@ transExpr state (MusAST.Rest duration) = do
 -- Sounding Notes
 ------------------------------------------------
 transExpr state (MusAST.Note (MusAST.Tone noteName accidental octave) duration) =
-  if octave < 1 || octave > 8 then return $ error "octave must be between 1 and 8 inclusive" else do
+    transExpr state (MusAST.Chord [MusAST.Tone noteName accidental octave] duration) -- MOVE THIS TO IR FILE 
+
+
+------------------------------------------------
+-- Chords
+------------------------------------------------
+transExpr state (MusAST.Chord tones duration) = do 
   let (currBeatCt, measureCt, keySig) = state
   measureNum                      <- IORef.readIORef measureCt
   currentBeatCount                <- IORef.readIORef currBeatCt
   (lastSharpMaybe, lastFlatMaybe) <- IORef.readIORef keySig
   noteDurationVal                 <- Bimap.lookup duration globalDurationIntBimap 
   let remainingTimeInMeasure = globalTimePerMeasure - currentBeatCount
-
-  let alterValue = case accidental of
-                    MusAST.Sharp -> 1
-                    MusAST.Flat -> -1
-                    MusAST.Natural -> 0
-
-  let noteTypeCode = durationToNoteTypeCode duration 
-      pitchCode = 
-        ["\t\t\t\t<pitch>",
-        "\t\t\t\t\t<step>" ++ show noteName ++ "</step>",
-        "\t\t\t\t\t<alter>" ++ show alterValue ++ "</alter>", --- REPLACE THIS WITH THE ACTUAL WHEN FIXED
-        "\t\t\t\t\t<octave>" ++ show octave ++ "</octave>",
-        "\t\t\t\t</pitch>"]
+      noteTypeCode = durationToNoteTypeCode duration 
+      pitchesCode = concat (zipWith
+        (\index (MusAST.Tone noteName accidental octave) ->
+          if octave < 1 || octave > 8 then return $ error "octave must be between 1 and 8 inclusive" else
+            let alterValue = case accidental of
+                  MusAST.Sharp -> 1
+                  MusAST.Flat -> -1
+                  MusAST.Natural -> 0
+                chordCode = if index > 0 then ["\t\t\t\t<chord/>"] else []
+            in return $ 
+            chordCode ++
+            ["\t\t\t\t<pitch>",
+            "\t\t\t\t\t<step>" ++ show noteName ++ "</step>",
+            "\t\t\t\t\t<alter>" ++ show alterValue ++ "</alter>", --- REPLACE THIS WITH THE ACTUAL WHEN FIXED
+            "\t\t\t\t\t<octave>" ++ show octave ++ "</octave>",
+            "\t\t\t\t</pitch>"]) [0..] tones)
 
   if noteDurationVal <= remainingTimeInMeasure -- note fits in measure
     then do 
       updateBeat noteDurationVal state -- update the beat, but there's no new measure code
       return $ -- the code for the note that fits in the current measure
-        ["\t\t\t<note>"]
-        ++ pitchCode ++
-        ["\t\t\t\t<duration>" ++ show noteDurationVal ++ "</duration>",
-        "\t\t\t\t<voice>1</voice>"]
-        ++ noteTypeCode ++
-        ["\t\t\t</note>"]
+        Prelude.concatMap 
+          (\pitchCode ->
+            ["\t\t\t<note>"]
+            ++ pitchCode ++
+            ["\t\t\t\t<duration>" ++ show noteDurationVal ++ "</duration>",
+            "\t\t\t\t<voice>1</voice>"]
+            ++ noteTypeCode ++
+            ["\t\t\t</note>"]) pitchesCode
 
   else do -- note does not fit in measure
     -- the code for note rest that fits in the current measure
@@ -229,18 +242,20 @@ transExpr state (MusAST.Note (MusAST.Tone noteName accidental octave) duration) 
     let (firstNoteDuration, firstNoteVal) = head initialNoteDivisions -- the first note of the tie only gets tied one way, from the right
         firstNoteTypeCode                 = durationToNoteTypeCode firstNoteDuration
         firstInitialNoteCode              = -- handle the first note of the tie sequence, which has a start tie ONLY
-          ["\t\t\t<note>"]
-            ++ pitchCode ++
-            ["\t\t\t\t<duration>" ++ show firstNoteVal ++ "</duration>", -- the duration of this note is all the time that's left in the measure, since the note doesn't fit in the measure
-            "\t\t\t\t<tie type=\"start\"/>", -- the note doesn't fit in the measure, so we tie
-            "\t\t\t\t<voice>1</voice>"]
-            ++ firstNoteTypeCode ++
-            ["\t\t\t\t<notations>",
-              "\t\t\t\t\t<tied type=\"start\"/>",
-            "\t\t\t\t</notations>",
-          "\t\t\t</note>"]
+          Prelude.concatMap 
+          (\pitchCode -> 
+            ["\t\t\t<note>"]
+              ++ pitchCode ++
+              ["\t\t\t\t<duration>" ++ show firstNoteVal ++ "</duration>", -- the duration of this note is all the time that's left in the measure, since the note doesn't fit in the measure
+              "\t\t\t\t<tie type=\"start\"/>", -- the note doesn't fit in the measure, so we tie
+              "\t\t\t\t<voice>1</voice>"]
+              ++ firstNoteTypeCode ++
+              ["\t\t\t\t<notations>",
+                "\t\t\t\t\t<tied type=\"start\"/>",
+              "\t\t\t\t</notations>",
+            "\t\t\t</note>"]) pitchesCode
         remainingInitialNoteDivisions = tail initialNoteDivisions
-    remainingInitialNoteCode <- generateTiedNotesFromDivisions pitchCode remainingInitialNoteDivisions -- generating the remaining tied notes that fit in this measure (each note is tied both ways)
+    remainingInitialNoteCode <- generateTiedNotesFromDivisions pitchesCode remainingInitialNoteDivisions -- generating the remaining tied notes that fit in this measure (each note is tied both ways)
     
     newMeasureCode <- updateBeat remainingTimeInMeasure state
 
@@ -251,33 +266,35 @@ transExpr state (MusAST.Note (MusAST.Tone noteName accidental octave) duration) 
     -- True bc start of measure, so we want rests from greatest -> least
     spilledNoteDivisions <- generateNoteValueRationalDivisions remainingNoteLength True 
     let (lastNoteDuration, lastNoteVal) = last spilledNoteDivisions -- the last note of the tie only gets tied one way, from the left
-        lastNoteTypeCode              = durationToNoteTypeCode lastNoteDuration
-        finalSpilledNoteCode           =  -- handle the final note of the tie sequence, which has a stop tie ONLY
-          ["\t\t\t<note>"]
-            ++ pitchCode ++
-            ["\t\t\t\t<duration>" ++ show lastNoteVal ++ "</duration>", -- the duration of this note is all the time that's left in the measure, since the note doesn't fit in the measure
-            "\t\t\t\t<tie type=\"stop\"/>", -- the note doesn't fit in the measure, so we tie
-            "\t\t\t\t<voice>1</voice>"]
-            ++ lastNoteTypeCode ++
-            ["\t\t\t\t<notations>",
-              "\t\t\t\t\t<tied type=\"stop\"/>",
-            "\t\t\t\t</notations>",
-          "\t\t\t</note>"]
-        remainingSpilledNoteDivisions = tail spilledNoteDivisions 
-    remainingSpilledNoteCode <- generateTiedNotesFromDivisions pitchCode remainingSpilledNoteDivisions
+        lastNoteTypeCode                = durationToNoteTypeCode lastNoteDuration
+        finalSpilledNoteCode            =  -- handle the final note of the tie sequence, which has a stop tie ONLY
+          Prelude.concatMap 
+            (\pitchCode -> 
+              ["\t\t\t<note>"]
+                ++ pitchCode ++
+                ["\t\t\t\t<duration>" ++ show lastNoteVal ++ "</duration>", -- the duration of this note is all the time that's left in the measure, since the note doesn't fit in the measure
+                "\t\t\t\t<tie type=\"stop\"/>", -- the note doesn't fit in the measure, so we tie
+                "\t\t\t\t<voice>1</voice>"]
+                ++ lastNoteTypeCode ++
+                ["\t\t\t\t<notations>",
+                  "\t\t\t\t\t<tied type=\"stop\"/>",
+                "\t\t\t\t</notations>",
+              "\t\t\t</note>"]) pitchesCode
+        remainingSpilledNoteDivisions   = tail spilledNoteDivisions 
+    remainingSpilledNoteCode <- generateTiedNotesFromDivisions pitchesCode remainingSpilledNoteDivisions
 
     updateBeat remainingNoteLength state -- with current time sig/note length setup, cannot have a tied note that fills the next measure, so NO new measure code should get generated here
 
-    return $ firstInitialNoteCode ++ remainingInitialNoteCode ++ newMeasureCode ++ remainingSpilledNoteCode ++ finalSpilledNoteCode    
+    return $ firstInitialNoteCode ++ remainingInitialNoteCode ++ newMeasureCode ++ remainingSpilledNoteCode ++ finalSpilledNoteCode   
 
 ------------------------------------------------
 -- Anything that remains untranslated prints a warning message
 -- When you think you're done, this should probably be replaced
 -- by a call to error.
 ------------------------------------------------
-transExpr _ _ = do
-  putStrLn "  unknown MusAST expr "
-  return []
+-- transExpr _ _ = do
+--   putStrLn "  unknown MusAST expr "
+--   return []
 
 -----------------------------------------------------------------------------------------
 -- Code Generation for Instructions
